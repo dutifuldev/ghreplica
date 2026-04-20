62fd4c06850970c0affb185e0f6ab5bfc83eb567
Bob <dutifulbob@gmail.com>
2026-04-20 13:00:07 +0200

    Add GitHub read path indexes

---
+++ migrations/000015_github_read_path_indexes.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_pull_requests_repo_state_created_at;
+DROP INDEX IF EXISTS idx_pull_requests_repo_created_at;
+DROP INDEX IF EXISTS idx_issues_repo_state_created_at;
+DROP INDEX IF EXISTS idx_issues_repo_created_at;
+DROP INDEX IF EXISTS idx_repositories_owner_login_name;
