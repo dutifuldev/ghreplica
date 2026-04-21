e85dfa91543a48918f0ed5bd02b6d17d37de0b4c
Bob <dutifulbob@gmail.com>
2026-04-21 18:02:55 +0200

    fix(migrations): split concurrent webhook index swap (#67)

--- migrations/000023_webhook_delivery_cleanup_index_swap.down.sql
+++ migrations/000023_webhook_delivery_cleanup_index_swap.down.sql
@@ -1,7 +1,3 @@
 -- ghreplica:nontransactional
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_webhook_deliveries_processed_at
-ON webhook_deliveries (processed_at)
-WHERE processed_at IS NOT NULL AND compacted_at IS NULL;
-
 DROP INDEX CONCURRENTLY IF EXISTS idx_webhook_deliveries_cleanup;
---
+++ migrations/000024_webhook_delivery_drop_prior_cleanup_index.up.sql
@@ -0,0 +1,3 @@
+-- ghreplica:nontransactional
+
+DROP INDEX CONCURRENTLY IF EXISTS idx_webhook_deliveries_processed_at;
