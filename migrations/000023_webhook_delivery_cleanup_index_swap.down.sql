87e730319e7bc468433c82d78415157bc1878fe1
Bob <dutifulbob@gmail.com>
2026-04-21 17:32:36 +0200

    fix(migrations): restore prior webhook index on rollback

--- migrations/000023_webhook_delivery_cleanup_index_swap.down.sql
+++ migrations/000023_webhook_delivery_cleanup_index_swap.down.sql
@@ -2,6 +2,6 @@
 
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_webhook_deliveries_processed_at
 ON webhook_deliveries (processed_at)
-WHERE processed_at IS NOT NULL;
+WHERE processed_at IS NOT NULL AND compacted_at IS NULL;
 
 DROP INDEX CONCURRENTLY IF EXISTS idx_webhook_deliveries_cleanup;
